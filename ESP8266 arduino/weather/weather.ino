2f91f78d09b5912dd8c98ad5901d9049e52634b3
soilderjames <soilderjames@163.com>
2021-10-15 18:39:03 +0800

    Create weather.ino
    ESP8266 ARDUINO

---
+++ ESP8266 arduino/weather/weather.ino
@@ -0,0 +1,50 @@
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+
+void weather()
+{
+  HTTPClient http;
+  WiFiClient client;
+  http.begin(client,"http://api.openweathermap.org/data/2.5/weather?id=(your city id here)&units=metric&APPID=(your api id here)");
+  int httpCode = http.GET();
+  if (httpCode > 0)
+{
+String payload = http.getString();
+Serial.printf(payload.c_str());
+/*DynamicJsonDocument doc(1024);
+DeserializationError error = deserializeJson(doc, payload);
+if (error) {
+  Serial.print(F("deserializeJson() failed: "));
+  Serial.println(error.f_str());
+  return;
+}
+const char* city = doc["name"];
+float temp = (float)(doc["main"]["temp"]);
+JsonObject weather_0 = doc["weather"][0];
+int weather_0_id = weather_0["id"];
+const char* weather_0_main = weather_0["main"];
+      Serial.printf("%s\n", city);
+      Serial.printf("%.2f°C\r\n", temp);      
+      Serial.printf("%s\n", weather_0_main);*/
+}
+}
+
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(115200);
+ WiFi.begin("qwj1", "88278791");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (WiFi.status() == WL_CONNECTED) 
+  {
+    if(Serial.find("GET WEATHER"))
+  {
+    weather();
+  while(Serial.read() >= 0){}
+}
+  }
+}
